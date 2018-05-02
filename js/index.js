7fef7b6bba7b4e66e90b873cd7d79cc9825c83d0
bethelight <1598204060@qq.com>
2018-05-02 19:28:40 +0800

    'shenmu-web-admin'

---
+++ js/index.js
@@ -0,0 +1,21 @@
+const header = `
+    <div class= "header">
+        <div class="left-top">神木app管理后台</div>
+        <div class="right-top">
+            <button id="btn1" class="btn btn-primary" style="font-size: 28px">上传新资讯</button>
+            <button id="btn2" class="btn btn-primary" style="font-size: 28px">App资讯列表</button>
+        </div>
+    </div>
+`;
+const footer = `
+    <div class="footer">
+        <span>版权所有 神木市政府 &copy 2018</span>
+    </div>
+`;
+$(function(){
+    $("header").html(header);
+    $("footer").html(footer);
+});
+function menuBtn(which) {
+
+}
---
+++ utils/templete.js
@@ -0,0 +1,12 @@
+// const header = `
+//     <div class= "header">
+//         <p>神木app管理后台</p>
+//         <button id="btn1" class="btn btn-group-lg">上传新资讯</button>
+//         <button id="btn2" class="btn btn-group-lg">App资讯上传列表</button>
+//     </div>
+// `;
+// const footer = `
+//     <div class="footer">
+//         <span>神木市政府</span>
+//     </div>
+// `;
